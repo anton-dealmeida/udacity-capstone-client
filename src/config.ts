b68e478a42603c24130431df2f9a3c8d09565706
Antonio De Almeida <anton@dealmeida.co>
2020-08-10 23:22:29 +0200

    Added serverless configuration for CI/CD pipeline

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const apiEndpoint = '...'
 
 export const authConfig = {
-  domain: '...',
+  domain: 'https://d1o1bo6bje38f6.cloudfront.net/',
   clientId: '...',
   callbackUrl: 'http://localhost:3000/callback'
 }
