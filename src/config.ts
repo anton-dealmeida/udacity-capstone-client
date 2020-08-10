a964360a99f4721a6bd9a93e8f7132890ed15db3
Antonio De Almeida <anton@dealmeida.co>
2020-08-11 01:28:37 +0200

    Updated with dynamic configurations.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = process.env.API_URL || "..."
+export const apiEndpoint = process.env.API_URL || "https://8em58ubnhl.execute-api.us-west-2.amazonaws.com/dev"
 
 export const authConfig = {
-  domain: process.env.AUTH_DOMAIN || '...',
-  clientId: process.env.AUTH_CLIENT_ID || '...',
+  domain: process.env.AUTH_DOMAIN || 'udagram-da-dev.eu.auth0.com',
+  clientId: process.env.AUTH_CLIENT_ID || 'j79RMSJbUX67eVViz7ZwoJXACjttic6C',
   callbackUrl: process.env.AUTH_CALLBACK_URL || 'http://localhost:3000/callback'
 }
